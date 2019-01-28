b02f3d14c74a1c9a81c170783f35ad185b5823f8
Mehmet Tuğrul Şahin <mehmettugrulsahin@gmail.com>
2019-01-28 23:33:47 +0100

    clean up a

--- packages/client/src/pattern/atom/ErrorBoundary/ErrorBoundary.js
+++ packages/client/src/pattern/atom/ErrorBoundary/ErrorBoundary.js
@@ -1,5 +1,4 @@
 /* eslint no-unused-vars : 0 */
-
 import React from 'react'
 import { node } from 'prop-types'
 
@@ -10,13 +9,11 @@ class ErrorBoundary extends React.Component {
   }
 
   static getDerivedStateFromError(error) {
-    // Update state so the next render will show the fallback UI.
     return { hasError: true }
   }
 
   render() {
     if (this.state.hasError) {
-      // You can render any custom fallback UI
       return <h1>Something went wrong.</h1>
     }
 
