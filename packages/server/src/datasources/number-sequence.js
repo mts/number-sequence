a6c8d4daa743299d38e044a9463a199135e8f829
Mehmet Tuğrul Şahin <mehmettugrulsahin@gmail.com>
2019-01-28 21:13:32 +0100

    calculate number sequences

--- packages/server/src/datasources/number-sequence.js
+++ packages/server/src/datasources/number-sequence.js
@@ -1,14 +1,14 @@
 /* eslint-disable */
 
 const sequenceSeedValue = 1
-const numberOfSequences = 10
+const numberOfSequences = 20
 
 function addSequence(sequences) {
-  const lastSequence = sequences[sequences.length - 1]
+  const lastSequence = sequences[sequences.length - 1].numbers
   const nextSequence = []
 
   if (sequences.length === 1) {
-    return sequences.push([sequences[0][0], sequences[0][0]])
+    return sequences.push({ numbers: [sequences[0].numbers[0], sequences[0].numbers[0]] })
   }
 
   for (let i = 0; i < lastSequence.length; ++i) {
@@ -26,12 +26,12 @@ function addSequence(sequences) {
     nextSequence.push(lastSequence[i - 1] + lastSequence[i])
   }
 
-  return sequences.push(nextSequence)
+  return sequences.push({ numbers: nextSequence })
 }
 
 function initSequences(sequenceSeedValue) {
   const firstSequence = [sequenceSeedValue]
-  return [firstSequence]
+  return [{ numbers: firstSequence }]
 }
 
 function extendSequences(sequences) {
@@ -43,11 +43,10 @@ function extendSequences(sequences) {
 }
 
 function consoleLogSequences(sequences) {
-  let padCharacter = ' '
+  let padCharacter = ', '
 
   for (let i = 0; i < sequences.length; ++i) {
     const paddedSequence = ''.padStart(sequences.length - i, ' ').concat(sequences[i].join(padCharacter))
-    // console.log(paddedSequence)
     console.log(sequences[i].join(padCharacter))
   }
 }
@@ -61,11 +60,12 @@ function displaySequences(sequenceSeedValue) {
   consoleLogSequences(sequences)
 }
 
-displaySequences(sequenceSeedValue)
+// displaySequences(sequenceSeedValue)
 
-export function getSequences(sequenceSeedValue) {
-  let sequences = initSequences(sequenceSeedValue)
-  return (sequences = extendSequences(sequences))
+module.exports = {
+  getSequences: function(sequenceSeedValue) {
+    let sequences = initSequences(sequenceSeedValue)
+    return extendSequences(sequences)
+  },
 }
-
 /* eslint-enable */
--- packages/server/src/datasources/sequence.js
+++ packages/server/src/datasources/sequence.js
@@ -1,9 +1,11 @@
 /* eslint class-methods-use-this: 0 */
 const { RESTDataSource } = require('apollo-datasource-rest')
-const { sequences } = require('./data.json')
+// const { sequences } = require('./data.json')
+const { getSequences } = require('./number-sequence')
 
 class SequenceAPI extends RESTDataSource {
   async getAllSequences() {
+    const sequences = getSequences(1)
     return sequences && sequences.length ? sequences.map(l => this.sequenceReducer(l)) : []
   }
 
